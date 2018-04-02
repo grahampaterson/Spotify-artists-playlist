e35449bc91c146fc24a07b972ab7b7ee36de44a3
grahampaterson <paterson.graham@gmail.com>
2018-04-02 16:29:10 +0200

    Testing React

---
+++ frontend-development/src/Components/Button.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  render() {
+    return (
+      <div className="Button">
+        This is a button
+      </div>
+    )
+  }
+}
+
+export default Button; // Don’t forget to use export default!
